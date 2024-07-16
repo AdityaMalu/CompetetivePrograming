a72e9cf6f9da3683abe5a4c71663978a2e84d9f8
Aditya Malu <C2K21107036@ms.pict.edu>
2024-07-16 10:42:21 +0530

    16-07-24

--- CodeForces/CodeForces_958/C_Increasing_Sequence_with_Fixed_OR.py
+++ CodeForces/CodeForces_958/C_Increasing_Sequence_with_Fixed_OR.py
@@ -1,29 +1,16 @@
 def solve():
-    n = int(input())
+    n = int(input().strip())
     ans = [n]
     
-    if n == 1:
-        print(1)
-        print(1)
-        return
-    
-    k = 0
-    mp1 = {}
-    for i in range(61):
-        if n & (1 << i):
-            mp1[k] = i
-            k += 1
-    
-    for i in range(k):
-        x = 0
-        for j in range(61):
-            if ((1 << k) - 1 - (1 << i)) & (1 << j):
-                x += (1 << mp1[j])
-        if x != 0:
-            ans.append(x)
+    bitw = 1
+    while bitw < n:
+        if(n & bitw):
+            ans.append(n - bitw)
+        bitw <<= 1
     
     print(len(ans))
-    print(*reversed(ans))
+    print(*sorted(ans))
+        
 
 def main():
     t = int(input())
